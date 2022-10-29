1762c492a1c302eceeae5871367e88c4d2a76120
Humzas <humzas@umich.edu>
2022-10-29 16:02:50 -0400

    relinked CSS structure to webpage and filled out README.MD

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var passwordLength = 0;
-
 // var incLC = false;
 // var incUC = false;
 // var incNum = false;
 // var incSpec = false;
 function generatePassword() {
+  //Defines default variables that will be changed within the scope of the program.
   var LC = 'N';
   var UC = 'N';
   var num = 'N';
   var spec = 'N';
   var genPassword = "";
   var charList = "";
-  passwordLength = window.prompt("Choose a password length between 8 and 128 characters.");
+  var passwordLength = window.prompt("Choose a password length between 8 and 128 characters.");
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     passwordLength = window.prompt("Please choose a valid password length between 8 and 128 characters")
   }
@@ -43,6 +42,7 @@ function generatePassword() {
       window.alert("Please select at least one character type to include in the password.") 
     }
   }
+  //This section adds the appropriate characters to the list of potential characters to be used in the generated password
   if (LC == "Y") {
     charList = charList.concat("abcdefghijklmnopqrstuvwxyz");
   }
@@ -53,8 +53,9 @@ function generatePassword() {
     charList = charList.concat("1234567890");
   }
   if (spec == "Y") {
-    charList = charList.concat("' !\"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~");
+    charList = charList.concat("' !\"#$%&'()*+,-./:;<=>?@[]\\^_`{|}~");
   }
+  // The created string of all the potential characters to be used in the password is used to generate a random password of specific length. 
   for (var i = 0; i < passwordLength; i++) {
     genPassword = genPassword.concat(charList[Math.floor(Math.random() * charList.length)]); 
   }
@@ -71,9 +72,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// generatePassword();
-// window.alert(passwordLength);
-// window.alert(incLC);
-// window.alert(incUC);
-// window.alert(incNum);
-// window.alert(incSpec);
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # PasswordGenerator
+
+## Description
+
+This is a password generator that functions to generate a psuedorandom password given requirements that the user is prompted for.  The password is displayed in the textbox and can be copied and pasted wherever it is needed.
+
+## Usage
+
+When the "Generate Password" button is pressed, a series of prompts will appear asking you questions about requirements for your password.  Once you choose a password length between 8 and 128 characters, the prompts will ask you if you would like to include lowercase letters, uppercase letters, numeric values, and special characters.  To select the criteria you would like to include reply "Y" or "N".  Once the criteria is filled out, you will be given a randomly generated password fulfilling that criteria.
+
+## Mock-Up
+
+Here is a screenshot of the deployed application.
+
+## Credits
+
+Starter code was found here [github](https://github.com/RutgersCodingBootcamp/RUT-VIRT-FSF-PT-10-2022-U-LOLC/tree/main/03-JavaScript/02-Challenge) at the Rutgers Coding Bootcamp Github Repository.
+
+## License
+
+MIT License
